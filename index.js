8ad778fcad78584357df3ab5bcfcc6a568d0770d
JECKS-SAMP <akeno.xyz@gmail.com>
2024-07-07 18:06:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,70 @@
-var dgram = require('dgram');
-var decode = require('iconv-lite').decode;
-//var {DgramAsPromised} = require("dgram-as-promised")
+const dgram = require('dgram');
+const decode = require('iconv-lite').decode;
 
-var query = function (options, callback)
-{
-  var self = this;
+const query = function (options, callback) {
+    var self = this;
 
-  if (typeof options === 'string') options.host = options;
-  options.port = options.port || 7777;
-  options.timeout = options.timeout || 2000;
+    if (typeof options === 'string') options.host = options;
+    options.port = options.port || 7777;
+    options.timeout = options.timeout || 5000; // Tingkatkan timeout menjadi 5000ms
 
-  if (!options.host) return callback.apply(options, ['Invalid host']);
+    if (!options.host) return callback.apply(options, ['Invalid host']);
 
-  if (
-    !isFinite(options.port) ||
-    options.port < 1 ||
-    options.port > 65535
-  )
-  return callback.apply(options, ['Invalid port']);
+    if (
+        !isFinite(options.port) ||
+        options.port < 1 ||
+        options.port > 65535
+    )
+        return callback.apply(options, ['Invalid port']);
 
-  var response = {};
+    var response = {};
+    var startTime = new Date().getTime();
 
-  var startTime = new Date().getTime();
+    request.call(self, options, 'i', function (error, information) {
+        if (error) return callback.apply(options, [error]);
 
-  request.call(self, options, 'i', function (error, information) {
-    if (error) return callback.apply(options, [error]);
+        response.address = options.host;
+        response.hostname = information.hostname;
+        response.gamemode = information.gamemode;
+        response.mapname = information.mapname;
+        response.passworded = information.passworded === 1;
+        response.maxplayers = information.maxplayers;
+        response.online = information.players;
 
-    response.address = options.host;
-    response.hostname = information.hostname;
-    response.gamemode = information.gamemode;
-    response.mapname = information.mapname;
-    response.passworded = information.passworded === 1;
-    response.maxplayers = information.maxplayers;
-    response.online = information.players;
+        request.call(self, options, 'r', function (error, rules) {
+            if (error) return callback.apply(options, [error]);
 
-    request.call(self, options, 'r', function (error, rules) {
-      if (error) return callback.apply(options, [error]);
+            rules.lagcomp = rules.lagcomp === 'On';
+            rules.weather = parseInt(rules.weather, 10);
 
-      rules.lagcomp = rules.lagcomp === 'On';
+            response.rules = rules;
 
-      rules.weather = parseInt(rules.weather, 10);
+            if (response.online > 100) {
+                response.players = [];
+                var endTime = new Date().getTime();
+                response.ping = endTime - startTime;
+                return callback.apply(options, [false, response]);
+            } else {
+                request.call(self, options, 'd', function (error, players) {
+                    if (error) return callback.apply(options, [error]);
 
-      response.rules = rules;
-
-      if (response.online > 100) {
-        response.players = [];
-        var endTime = new Date().getTime();
-        response.ping = endTime - startTime;
-        return callback.apply(options, [false, response]);
-      } else {
-        request.call(self, options, 'd', function (error, players) {
-          if (error) return callback.apply(options, [error]);
-
-          response.players = players;
-          var endTime = new Date().getTime();
-          response.ping = endTime - startTime;
-          return callback.apply(options, [false, response]);
+                    response.players = players;
+                    var endTime = new Date().getTime();
+                    response.ping = endTime - startTime;
+                    return callback.apply(options, [false, response]);
+                });
+            }
         });
-      }
     });
-  });
 };
 
-var request = function(options, opcode, callback) {
+const request = function (options, opcode, callback) {
+    const socket = dgram.createSocket("udp4");
+    const packet = Buffer.alloc(11);
 
-    var socket = dgram.createSocket("udp4")
-    var packet = Buffer.alloc(11);
-
-    packet.write('SAMP')
+    packet.write('SAMP');
 
     const hostParts = options.host.split('.').map(Number);
-
     if (hostParts.length !== 4) {
         return callback.apply(options, [new Error('Invalid host format')]);
     }
@@ -82,286 +76,108 @@ var request = function(options, opcode, callback) {
     packet[9] = (options.port >> 8) & 0xFF;
     packet[10] = opcode.charCodeAt(0);
 
-    var controller = undefined;
+    console.log(`Sending packet to ${options.host}:${options.port} with opcode ${opcode}`);
 
-    var onTimeOut = function() {
+    try {
+        socket.send(packet, 0, packet.length, options.port, options.host, function (error, bytes) {
+            if (error) {
+                console.log(`Error sending packet: ${error}`);
+                return callback(error);
+            }
+            console.log(`Packet sent, ${bytes} bytes`);
+        });
+    } catch (error) {
+        console.log(`Exception during socket send: ${error}`);
+        return callback(error);
+    }
+
+    var controller = setTimeout(function () {
         socket.close();
-        return callback.apply(options, ['Host unavailable']);
-    };
+        console.log('Request timed out');
+        return callback('Host unavailable');
+    }, options.timeout);
 
-    controller = setTimeout(onTimeOut, options.timeout || 2000);
-
-    socket.on('message', function(message) {
-        if (controller) clearTimeout(controller);
+    socket.on('message', function (message) {
+        clearTimeout(controller);
+        socket.close();
 
         if (message.length < 11) return callback.apply(options, [true]);
-        else {
-            socket.close();
 
-            message = message.slice(11);
+        message = message.slice(11);
+        var object = {};
+        var array = [];
+        var strlen = 0;
+        var offset = 0;
 
-            var object = {};
-            var array = [];
-            var strlen = 0;
-            var offset = 0;
-
-            try {
-                if (opcode == 'i') {               
-
-                    object.passworded = message.readUInt8(offset);
-                    offset += 1;
-
-                    object.players = message.readUInt16LE(offset);
-                    offset += 2;
-
-                    object.maxplayers = message.readUInt16LE(offset);
-                    offset += 2;
-
-                    strlen = message.readUInt16LE(offset);
-                    offset += 4;
-
-                    object.hostname = decode(message.slice(offset, offset += strlen));
-
-                    strlen = message.readUInt16LE(offset);
-                    offset += 4;
-
-                    object.gamemode = decode(message.slice(offset, offset += strlen));
-
-                    strlen = message.readUInt16LE(offset);
-                    offset += 4;
-
-                    object.mapname = decode(message.slice(offset, offset += strlen));
-
-                    return callback.apply(options, [false, object]);
-
-                }
-
-                if (opcode == 'r') {
-
-                    var rulecount = message.readUInt16LE(offset);
-                    offset += 2;
-
-                    var property, value;
-
-                    while (rulecount) {
-
-                        strlen = message.readUInt8(offset);
-                        ++offset;
-
-                        property = decode(message.slice(offset, offset += strlen));
-
-                        strlen = message.readUInt8(offset);
-                        ++offset;
-
-                        value = decode(message.slice(offset, offset += strlen));
-
-                        object[property] = value;
-
-                        --rulecount;
-                    }
-
-                    return callback.apply(options, [false, object]);
-                }
-
-                if (opcode == 'd') {
-
-                    var playercount = message.readUInt16LE(offset);
-                    offset += 2;
-
-                    var player;
-
-                    while (playercount) {
-
-                        player = {};
-
-                        player.id = message.readUInt8(offset);
-                        ++offset;
-
-                        strlen = message.readUInt8(offset);
-                        ++offset;
-
-                        player.name = decode(message.slice(offset, offset += strlen));
-
-                        player.score = message.readUInt16LE(offset);
-                        offset += 4;
-
-                        player.ping = message.readUInt16LE(offset);
-                        offset += 4;
-
-                        array.push(player);
-
-                        --playercount;
-                    }
-
-                    return callback.apply(options, [false, array]);
-                }
-
-            } catch (exception) {
-                return callback.apply(options, [exception]);
+        try {
+            if (opcode == 'i') {
+                object.passworded = message.readUInt8(offset);
+                offset += 1;
+                object.players = message.readUInt16LE(offset);
+                offset += 2;
+                object.maxplayers = message.readUInt16LE(offset);
+                offset += 2;
+                strlen = message.readUInt16LE(offset);
+                offset += 4;
+                object.hostname = decode(message.slice(offset, offset += strlen));
+                strlen = message.readUInt16LE(offset);
+                offset += 4;
+                object.gamemode = decode(message.slice(offset, offset += strlen));
+                strlen = message.readUInt16LE(offset);
+                offset += 4;
+                object.mapname = decode(message.slice(offset, offset += strlen));
+                return callback.apply(options, [false, object]);
             }
+
+            if (opcode == 'r') {
+                var rulecount = message.readUInt16LE(offset);
+                offset += 2;
+                var property, value;
+                while (rulecount) {
+                    strlen = message.readUInt8(offset);
+                    ++offset;
+                    property = decode(message.slice(offset, offset += strlen));
+                    strlen = message.readUInt8(offset);
+                    ++offset;
+                    value = decode(message.slice(offset, offset += strlen));
+                    object[property] = value;
+                    --rulecount;
+                }
+                return callback.apply(options, [false, object]);
+            }
+
+            if (opcode == 'd') {
+                var playercount = message.readUInt16LE(offset);
+                offset += 2;
+                var player;
+                while (playercount) {
+                    player = {};
+                    player.id = message.readUInt8(offset);
+                    ++offset;
+                    strlen = message.readUInt8(offset);
+                    ++offset;
+                    player.name = decode(message.slice(offset, offset += strlen));
+                    player.score = message.readUInt16LE(offset);
+                    offset += 4;
+                    player.ping = message.readUInt16LE(offset);
+                    offset += 4;
+                    array.push(player);
+                    --playercount;
+                }
+                return callback.apply(options, [false, array]);
+            }
+        } catch (exception) {
+            console.log('Exception during parsing:', exception);
+            return callback.apply(options, [exception]);
         }
     });
 
-    socket.on('error', function(err) {
+    socket.on('error', function (err) {
         clearTimeout(controller);
         socket.close();
+        console.log(`Socket error: ${err}`);
         return callback.apply(options, [err]);
     });
 };
 
-    //var socket = DgramAsPromised.createSocket("udp4")
-    //var packet = new Buffer(10 + opcode.length)
-
-    /*for(var i = 0; i < 4; ++i) 
-    packet[i + 4] = options.host.split('.')[i]
-
-    packet[8] = options.port & 0xFF
-    packet[9] = options.port >> 8 & 0xFF
-    packet[10] = opcode.charCodeAt(0)
-
-    try {
-        socket.send(packet, 0, packet.length, options.port, options.host, function(error, bytes) {
-            if(error) return callback.apply(options, [ error ])
-        })
-    } catch(error) {
-        return callback.apply(options, [ error ])
-    }
-
-    var controller = undefined
-
-    var onTimeOut = function() {
-        socket.close()
-        return callback.apply(options, [ 'Host unavailable' ])
-    }
-
-    controller = setTimeout(onTimeOut, options.timeout)
-
-    socket.on('message', function (message) {
-
-        if(controller)
-            clearTimeout(controller)
-
-        if(message.length < 11) return callback.apply(options, [ true ])
-        else {
-            socket.close()
-
-            message = message.slice(11)
-
-            var object = {}
-            var array = []
-            var strlen = 0
-            var offset = 0
-
-            try {
-
-                if(opcode == 'i') {               
-
-                    object.passworded = message.readUInt8(offset)
-                    offset += 1
-
-                    object.players = message.readUInt16LE(offset)
-                    offset += 2
-
-                    object.maxplayers = message.readUInt16LE(offset)
-                    offset += 2
-
-                    strlen = message.readUInt16LE(offset)
-                    offset += 4
-
-                    object.hostname = decode(message.slice(offset, offset += strlen))
-
-                    strlen = message.readUInt16LE(offset)
-                    offset += 4
-
-                    object.gamemode = decode(message.slice(offset, offset += strlen))
-
-                    strlen = message.readUInt16LE(offset)
-                    offset += 4
-
-                    object.mapname = decode(message.slice(offset, offset += strlen))
-
-                    return callback.apply(options, [ false, object ])
-
-                }
-
-                if(opcode == 'r') {
-
-                    var rulecount  = message.readUInt16LE(offset)
-                    offset += 2
-
-                    var property, value = undefined
-
-                    while(rulecount) {
-
-                        strlen = message.readUInt8(offset)
-                        ++offset
-
-                        property = decode(message.slice(offset, offset += strlen))
-
-                        strlen = message.readUInt8(offset)
-                        ++offset
-
-                        value = decode(message.slice(offset, offset += strlen))
-
-                        object[property] = value
-
-                        --rulecount
-                    }
-
-                    return callback.apply(options, [ false, object ])
-                }
-
-                if(opcode == 'd') {
-
-                    var playercount = message.readUInt16LE(offset)
-                    offset += 2
-
-                    var player = undefined;
-
-                    while(playercount) {
-
-                        player = {}
-
-                        player.id = message.readUInt8(offset)
-                        ++offset
-
-                        strlen = message.readUInt8(offset)
-                        ++offset
-
-                        player.name = decode(message.slice(offset, offset += strlen))
-
-                        player.score = message.readUInt16LE(offset)
-                        offset += 4
-
-                        player.ping = message.readUInt16LE(offset)
-                        offset += 4
-
-                        array.push(player)
-
-                        --playercount
-                    }
-
-                    return callback.apply(options, [ false, array ])
-                }
-
-            } catch (exception) {
-                return callback.apply(options, [ exception ])
-            }
-        }
-    })
-}*/
-
-var decode = function(buffer) {
-    var charset = ''
-    for (var i = 0; i < 128; i++) charset += String.fromCharCode(i)
-    charset += '€�‚ƒ„…†‡�‰�‹�����‘’“”•–—�™�›���� ΅Ά£¤¥¦§¨©�«¬­®―°±²³΄µ¶·ΈΉΊ»Ό½ΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡ�ΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώ�'
-    var charsetBuffer = Buffer.from(charset, 'ucs2')
-    var decodeBuffer = new Buffer(buffer.length * 2)
-    for(var i = 0; i < buffer.length; i++) {
-        decodeBuffer[i * 2] = charsetBuffer[buffer[i] * 2]
-        decodeBuffer[i * 2 + 1] = charsetBuffer[buffer[i] * 2 + 1]
-    }
-    return decodeBuffer.toString('ucs2')
-}
-
-module.exports = query
+module.exports = query;
